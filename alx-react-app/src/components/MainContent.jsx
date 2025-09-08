3250b159b1dd718cd6c376e5925f3d921a4eef72
Uche Bridget <uchebridget51@gmail.com>
2025-09-09 00:09:42 +0100

    Add styled components with inline CSS

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main>
+    <main style={{ padding: '20px', fontSize: '18px', color: 'darkgreen' }}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
