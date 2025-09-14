35afd86de581d97391f22e8eb072c604ca93ef27
Uche Bridget <uchebridget51@gmail.com>
2025-09-14 03:30:20 +0100

    Styled UserProfile with span

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
+function UserProfile() {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <span style={{ 
+        color: "blue", 
+        border: "1px solid blue", 
+        padding: "10px", 
+        margin: "10px" 
+      }}>
+        Styled text
+      </span>
     </div>
   );
-};
+}
 
 export default UserProfile;
