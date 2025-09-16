bacd9772db6718c66b39ac9f9b7cdab25755bb70
Uche Bridget <uchebridget51@gmail.com>
2025-09-16 05:00:42 +0100

    Add Router setup in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeForm from "./components/RecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
+    <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<RecipeForm />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
