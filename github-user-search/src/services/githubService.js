b76c973352856be5d7a34b681bc1b6221450d798
Uche Bridget <uchebridget51@gmail.com>
2025-09-18 01:48:06 +0100

    Fix: add exact GitHub search API URL

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,26 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
-
 export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(${BASE_URL}/users/${username});
-    return response.data;
+    const res = await axios.get(https://api.github.com/users/${username});
+    return res.data;
   } catch (error) {
     console.error("Error fetching user data:", error);
     throw error;
   }
 };
 
-export const fetchAdvancedUsers = async (username, location, minRepos) => {
+export const fetchAdvancedUsers = async (query) => {
   try {
-    let query = username ? ${username} in:login : "";
-
-    if (location) query += ` location:${location}`;
-    if (minRepos) query += ` repos:>=${minRepos}`;
-
-    const response = await axios.get(${BASE_URL}/search/users?q=${query});
-    return response.data.items;
+    const url = https://api.github.com/search/users?q=${query};
+    const res = await axios.get(url);
+    return res.data;
   } catch (error) {
     console.error("Error fetching advanced users:", error);
     throw error;
